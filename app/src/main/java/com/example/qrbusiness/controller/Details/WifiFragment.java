3117ca7c50b5fe9a4cf996c0fdf69815c0bca8bf
Mikkel P <mikkel-pieler@hotmail.com>
2019-05-21 20:57:38 +0200

    Create fragments added
    Missing:
      - Connection to REST service
      - Save functionality for created QR

---
+++ app/src/main/java/com/example/qrbusiness/controller/Details/WifiFragment.java
@@ -0,0 +1,91 @@
+package com.example.qrbusiness.controller.Details;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.qrbusiness.R;
+import com.example.qrbusiness.model.ORModels.QRWiFi;
+import com.squareup.picasso.Picasso;
+
+public class WifiFragment extends Fragment
+{
+    private WifiFragment.Callbacks detailsActivity;
+    private QRWiFi wifi;
+
+    private TextView tvName;
+    private TextView tvType;
+    private TextView tvWifiName;
+    private TextView tvPassword;
+    private TextView tvNetType;
+    private ImageView poster;
+
+    public WifiFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Bundle b = getArguments();
+        if (b != null && b.containsKey(wifi.WIFI_NAME))
+        {
+            wifi = new QRWiFi(b);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.details_wifi, container, false);
+
+        if (wifi != null)
+        {
+            this.tvName = view.findViewById(R.id.nameResult);
+            this.tvType = view.findViewById(R.id.typeResult);
+            this.tvWifiName = view.findViewById(R.id.wifiNameResult);
+            this.tvPassword = view.findViewById(R.id.passwordResult);
+            this.tvNetType = view.findViewById(R.id.netTypeResult);
+            this.poster = view.findViewById(R.id.poster);
+
+            tvName.setText(wifi.getName());
+            tvType.setText(wifi.getQrType());
+            tvWifiName.setText(wifi.getWifiName());
+            tvPassword.setText(wifi.getPassword());
+            tvNetType.setText(wifi.getNetType());
+
+            String image = wifi.getImagePath();
+
+            Picasso.with(getContext()).load(wifi.getImagePath()).into(poster);
+        }
+
+        return view;
+    }
+
+    public interface Callbacks
+    {
+        public void onAddToListClicked(QRWiFi wiFi);
+    }
+
+    public void btnOnQRAdded()
+    {
+        detailsActivity.onAddToListClicked(this.wifi);
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        this.detailsActivity = (WifiFragment.Callbacks) activity;
+    }
+}
